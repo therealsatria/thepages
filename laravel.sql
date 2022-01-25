e2c696d12d644ddee7145cac7af5601e1e44ad52
rsmhdev <rsmhwebdev@gmail.com>
2022-01-25 09:22:04 +0700

    satria

--- laravel.sql
+++ laravel.sql
@@ -11,57 +11,11 @@
  Target Server Version : 100138
  File Encoding         : 65001
 
- Date: 16/01/2022 19:20:59
+ Date: 25/01/2022 09:21:54
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for employees
--- ----------------------------
-DROP TABLE IF EXISTS `employees`;
-CREATE TABLE `employees`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nama` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `jk` enum('L','P') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telp` bigint NOT NULL,
-  `created_at` timestamp(0) NULL DEFAULT NULL,
-  `updated_at` timestamp(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Records of employees
--- ----------------------------
-INSERT INTO `employees` VALUES (13, 'char 1', 'L', 0, '2021-07-10 15:58:02', '2021-12-26 13:48:19');
-INSERT INTO `employees` VALUES (14, 'char sk', 'P', 6844637, '2021-07-28 01:51:51', '2021-10-02 06:32:44');
-INSERT INTO `employees` VALUES (15, 'aaaaa', 'L', 3131251212, '2021-12-23 09:29:37', '2021-12-23 09:31:52');
-INSERT INTO `employees` VALUES (16, 'dsdfasd', 'L', 234234, '2021-12-27 15:10:35', '2021-12-27 15:10:35');
-INSERT INTO `employees` VALUES (17, 'grtjrth', 'L', 43634, '2021-12-27 15:10:45', '2021-12-27 15:10:45');
-INSERT INTO `employees` VALUES (18, 'sdfgswhwrhw', 'L', 535734563, '2021-12-27 15:11:01', '2021-12-27 15:11:01');
-INSERT INTO `employees` VALUES (19, 'ghghghgh', 'P', 3573456346, '2021-12-27 15:11:18', '2021-12-27 15:11:18');
-INSERT INTO `employees` VALUES (20, 'mmmmmmmm', 'P', 55555, '2021-12-27 15:11:31', '2022-01-10 15:41:02');
-
--- ----------------------------
--- Table structure for failed_jobs
--- ----------------------------
-DROP TABLE IF EXISTS `failed_jobs`;
-CREATE TABLE `failed_jobs`  (
-  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-  PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `failed_jobs_uuid_unique`(`uuid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Records of failed_jobs
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for migrations
@@ -72,7 +26,7 @@ CREATE TABLE `migrations`  (
   `migration` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of migrations
@@ -81,21 +35,27 @@ INSERT INTO `migrations` VALUES (1, '2014_10_12_000000_create_users_table', 1);
 INSERT INTO `migrations` VALUES (2, '2014_10_12_100000_create_password_resets_table', 1);
 INSERT INTO `migrations` VALUES (3, '2019_08_19_000000_create_failed_jobs_table', 1);
 INSERT INTO `migrations` VALUES (4, '2021_07_07_090044_create_employees_table', 1);
+INSERT INTO `migrations` VALUES (5, '2022_01_25_020450_pages', 2);
 
 -- ----------------------------
--- Table structure for password_resets
+-- Table structure for pages
 -- ----------------------------
-DROP TABLE IF EXISTS `password_resets`;
-CREATE TABLE `password_resets`  (
-  `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `subject` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `author` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp(0) NULL DEFAULT NULL,
-  INDEX `password_resets_email_index`(`email`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
+  `updated_at` timestamp(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Records of password_resets
+-- Records of pages
 -- ----------------------------
+INSERT INTO `pages` VALUES (1, 'ujicoba 1 2', 'test 1 2', 'satria', '2022-01-25 02:19:41', '2022-01-25 02:19:57');
+INSERT INTO `pages` VALUES (2, 'ujicoba 2', 'test 2', 'satria', '2022-01-25 02:19:49', '2022-01-25 02:19:49');
 
 -- ----------------------------
 -- Table structure for users
